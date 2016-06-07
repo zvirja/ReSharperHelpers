37b7290eefdb5fe4cdd0735b308c0bf8095f4e56
Alex Povar <user.home.0000@gmail.com>
2016-06-07 11:48:48 +0300

    Add copy class name action

---
+++ AlexPovar.ResharperTweaks/ContextActions/CopyFullClassNameAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using JetBrains.Annotations;
+using JetBrains.Application.Progress;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.ContextActions;
+using JetBrains.ReSharper.Feature.Services.CSharp.Analyses.Bulbs;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Resources.Shell;
+using JetBrains.TextControl;
+using JetBrains.UI;
+using JetBrains.Util;
+
+namespace AlexPovar.ResharperTweaks.ContextActions
+{
+  [ContextAction(Group = "C#", Name = "Copy full class name", Description = "Copy full class name.",
+    Priority = short.MinValue)]
+  public class CopyFullClassNameAction : ContextActionBase
+  {
+    [NotNull] private readonly Clipboard _clipboard;
+    [NotNull] private readonly ICSharpContextActionDataProvider _provider;
+
+    public CopyFullClassNameAction([NotNull] ICSharpContextActionDataProvider provider)
+    {
+      if (provider == null) throw new ArgumentNullException(nameof(provider));
+
+      _provider = provider;
+      _clipboard = Shell.Instance.GetComponent<Clipboard>().NotNull("Unable to resolve clipboard service.");
+    }
+
+    public override string Text => "[Tweaks] Copy full class name";
+
+    protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+    {
+      var classDeclaration = _provider.GetSelectedElement<ICSharpIdentifier>()?.Parent as IClassDeclaration;
+      var declaredClass = classDeclaration?.DeclaredElement;
+
+      if (declaredClass == null) return null;
+
+      var typeName = declaredClass.GetClrName().FullName;
+      var moduleName = declaredClass.Module.Name;
+
+      var fullName = $"{typeName}, {moduleName}";
+      _clipboard.SetText(fullName);
+
+      return null;
+    }
+
+    public override bool IsAvailable(IUserDataHolder cache)
+    {
+      var classDeclaration = _provider.GetSelectedElement<ICSharpIdentifier>()?.Parent as IClassDeclaration;
+      return classDeclaration != null;
+    }
+  }
+}
