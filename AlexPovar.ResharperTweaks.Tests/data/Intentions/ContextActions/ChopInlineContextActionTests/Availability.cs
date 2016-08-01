5ec3cf2d2cf8b898492feb6ba98958a1fb8bb004
Alex Povar <zvirja1@gmail.com>
2016-08-02 00:05:12 +0300

    Add tests for Chop/Inline action

---
+++ AlexPovar.ResharperTweaks.Tests/data/Intentions/ContextActions/ChopInlineContextActionTests/Availability.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexPovar.ResharperTweaks.Tests.data.Intentions.ContextActions.ChopInlineContextActionTests
+{
+  class Availability
+  {
+    public Availa{off}bility()
+    {
+    }
+
+    public Availa{on}bility(int arg1)
+    {
+    }
+
+    void MethodWithout{off}Args()
+    {
+    }
+
+    void MethodWith{on}SingleArg(string arg1)
+    {
+    }
+
+    void MethodWith{on}Args(int arg1, string arg2)
+    {
+    }
+  }
+}
---
+++ AlexPovar.ResharperTweaks.Tests/data/Intentions/ContextActions/ChopInlineContextActionTests/MethodChoppedCorrectly.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexPovar.ResharperTweaks.Tests.data.Intentions.ContextActions.ChopInlineContextActionTests
+{
+  class MethodChoppedCorrectly
+  {
+    void Mixed{caret:Chop:method:arguments}Arguments(int arg1,
+  byte arg2, string arg3,
+          long arg4)
+    {
+    }
+  }
+}
