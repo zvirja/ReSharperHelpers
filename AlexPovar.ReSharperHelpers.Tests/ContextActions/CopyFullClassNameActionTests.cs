a264f96319f886960e1786997c1354bcd0025733
Alex Povar <zvirja1@gmail.com>
2016-08-29 00:47:10 +0300

    Add unit tests for CopyToClipboard action

--- AlexPovar.ReSharperHelpers.Tests/ContextActions/CopyFullClassNameActionTests.cs
+++ AlexPovar.ReSharperHelpers.Tests/ContextActions/CopyFullClassNameActionTests.cs
@@ -6,5 +6,13 @@ namespace AlexPovar.ReSharperHelpers.Tests.ContextActions
   public class CopyFullClassNameActionTests : PathedContextActionExecuteTestBase<CopyFullClassNameAction>
   {
     [Test] public void FileIsNotChanged() { this.DoNamedTest(); }
+
+    [Test] public void FullClassNameIsCopied()
+    {
+      this.DoNamedTest();
+
+      var currentClipboardText = System.Windows.Forms.Clipboard.GetText();
+      Assert.That(currentClipboardText, Is.EqualTo("AlexPovar.ReSharperHelpers.Tests.data.Intentions.ContextActions.CopyFullClassNameActionTests.FullClassNameIsCopied, TestProject"));
+    }
   }
 }
