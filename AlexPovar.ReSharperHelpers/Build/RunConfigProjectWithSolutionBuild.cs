e0f82339a4c2ec820312bfa5c182db1ff6f01f0b
Alex Povar <user.home.0000@gmail.com>
2016-10-12 19:19:52 +0300

    Add feature to add custom build configuration which triggers solution build before run

---
+++ AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBuild.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.IDE.RunConfig;
+
+namespace AlexPovar.ReSharperHelpers.Build
+{
+  public class RunConfigProjectWithSolutionBuild : RunConfigBase
+  {
+    public override void Execute(RunConfigContext context)
+    {
+      context.ExecutionProvider.Execute(null);
+    }
+  }
+}
---
+++ AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBulidProvider.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Application;
+using JetBrains.IDE.RunConfig;
+using JetBrains.UI.Icons;
+using JetBrains.VsIntegration.Resources;
+
+namespace AlexPovar.ReSharperHelpers.Build
+{
+  [ShellComponent]
+  public class RunConfigProjectWithSolutionBulidProvider : RunConfigProviderBase
+  {
+    public override string Name => "Launch solution with build config";
+
+    public override string Type => "solution with build config";
+
+    public override IconId IconId => RunConfigThemedIcons.RunConfigProject.Id;
+
+    public override IRunConfig CreateNew()
+    {
+      return new RunConfigProjectWithSolutionBuild
+      {
+        Type = this.Type
+      };
+    }
+  }
+}
