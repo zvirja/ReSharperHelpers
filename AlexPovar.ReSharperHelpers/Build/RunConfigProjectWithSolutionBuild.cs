1a05a6e5097a427e4c8283ee655f41b163112477
Alex Povar <user.home.0000@gmail.com>
2016-10-13 14:05:10 +0300

    Hide irrelevant options from build action configuration

--- AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBuild.cs
+++ AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBuild.cs
@@ -1,4 +1,8 @@
-﻿using JetBrains.IDE.RunConfig;
+﻿using System.Windows;
+using JetBrains.DataFlow;
+using JetBrains.IDE.RunConfig;
+using JetBrains.ProjectModel;
+using JetBrains.VsIntegration.IDE.RunConfig;
 
 namespace AlexPovar.ReSharperHelpers.Build
 {
@@ -7,6 +11,22 @@ namespace AlexPovar.ReSharperHelpers.Build
     public override void Execute(RunConfigContext context)
     {
       context.ExecutionProvider.Execute(null);
+    }
+
+    public override IRunConfigEditorAutomation CreateEditor(Lifetime lifetime, IRunConfigCommonAutomation commonEditor, ISolution solution)
+    {
+      //Configure commot editor to hide build options. We support solution build only.
+      commonEditor.IsWholeSolutionChecked.Value = true;
+      commonEditor.WholeSolutionVisibility.Value = Visibility.Collapsed;
+      commonEditor.IsSpecificProjectChecked.Value = false;
+
+      var casted = commonEditor as RunConfigCommonAutomation;
+      if (casted != null)
+      {
+        casted.ProjectRequiredVisibility.Value = Visibility.Collapsed;
+      }
+
+      return null;
     }
   }
 }
