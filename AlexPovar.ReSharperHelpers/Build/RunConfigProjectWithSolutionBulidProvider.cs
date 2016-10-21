6152e0f7cf534809d96ce6ec32a3bdee8665752c
Alex Povar <zvirja1@gmail.com>
2016-10-22 01:23:39 +0300

    Rename solution build action

--- AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBulidProvider.cs
+++ AlexPovar.ReSharperHelpers/Build/RunConfigProjectWithSolutionBulidProvider.cs
@@ -8,9 +8,9 @@ namespace AlexPovar.ReSharperHelpers.Build
   [ShellComponent]
   public class RunConfigProjectWithSolutionBulidProvider : RunConfigProviderBase
   {
-    public override string Name => "Launch solution with build config";
+    public override string Name => "Launch solution";
 
-    public override string Type => "solution with build config";
+    public override string Type => "launch solution";
 
     public override IconId IconId => RunConfigThemedIcons.RunConfigProject.Id;
 
