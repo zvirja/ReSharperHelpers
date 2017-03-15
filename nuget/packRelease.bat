3932c9e263a65b25e55f0f72779321b44adde444
Alex Povar <zvirja1@gmail.com>
2017-03-15 22:47:26 +0200

    Ensure any VS 2017 edition is detected

--- nuget/packRelease.bat
+++ nuget/packRelease.bat
@@ -1,2 +1,8 @@
-"c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" ..\code\AlexPovar.ReSharperHelpers.sln  /t:Build /p:Configuration=Release
+@echo off
+
+for /f "usebackq tokens=1* delims=: " %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+"%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" ..\code\AlexPovar.ReSharperHelpers.sln  /t:Build /p:Configuration=Release
 nuget pack ..\code\AlexPovar.ReSharperHelpers.nuspec -BasePath ..\code\AlexPovar.ReSharperHelpers\bin\Release\net45
