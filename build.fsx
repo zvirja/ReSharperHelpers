7675b592932a87261175d5a774ae0bf24564697a
Alex Povar <zvirja1@gmail.com>
2017-07-18 12:33:07 +0300

    Update build version on AppVeyor

--- build.fsx
+++ build.fsx
@@ -121,13 +121,20 @@ Target "PublishNuGetPublic" (fun _ -> publishNuget
 Target "PublishNuGetPrivate" (fun _ -> publishNuget 
                                          "https://www.myget.org/F/alexpovar-resharperhelpers-prerelease/api/v2/package" 
                                          (getBuildParam "NuGetPrivateKey") )
+// ==============================================
+// ================== AppVeyor ==================
+// ==============================================
+
+// Add helper to identify whether current trigger is PR
+type AppVeyorEnvironment with
+    static member IsPullRequest = isNotNullOrEmpty AppVeyorEnvironment.PullRequestNumber
 
 type AppVeyorTrigger = Invalid | SemVerTag | PR | DevelopBranch | UnknownBranchOrTag
 let appVeyorTrigger =
     if buildServer <> BuildServer.AppVeyor then
         Invalid
     else
-        let isPR = isNotNullOrEmpty AppVeyorEnvironment.PullRequestNumber
+        let isPR = AppVeyorEnvironment.IsPullRequest
         let isTag = AppVeyorEnvironment.RepoTag
         let isSemVerTag = isTag && "^v[\d\.]+" >** AppVeyorEnvironment.RepoTagName
         let branchName = AppVeyorEnvironment.RepoBranch
@@ -155,7 +162,9 @@ Target "AppVeyor_PublishTestResults" (fun _ ->
     if TestFile testResultFile then UploadTestResultsFile NUnit testResultFile
 )
 
-Target "AppVeyor" DoNothing
+Target "AppVeyor" (fun _ ->
+    if not AppVeyorEnvironment.IsPullRequest then UpdateBuildVersion currentBuildVersion.fileVersion
+)
 
 "Clean"
     ==> "NuGetRestore"
@@ -183,4 +192,5 @@ dependency "AppVeyor" <| match appVeyorTrigger with
 "AppVeyor_DescribeState" ?=> "AppVeyor_InvalidTrigger"
 "AppVeyor_DescribeState" ==> "AppVeyor"
 
+// ========= ENTRY POINT =========
 RunTargetOrDefault "CompleteBuild"
