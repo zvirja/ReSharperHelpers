eef95aede0ea2b2cad3e1d8553a08d1686213a9f
Alex Povar <zvirja1@gmail.com>
2018-12-27 12:47:32 +0200

    Use latest FAKE.Core 4.x version

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ cls
 
 IF NOT EXIST "build\tools\FAKE.Core\tools\Fake.exe" (
   "tools\nuget\nuget.exe" install "NUnit.Runners" -OutputDirectory "build\tools" -ExcludeVersion -Version 2.6.4
-  "tools\nuget\nuget.exe" install "FAKE.Core" -OutputDirectory "build\tools" -ExcludeVersion -Version 4.60.0
+  "tools\nuget\nuget.exe" install "FAKE.Core" -OutputDirectory "build\tools" -ExcludeVersion -Version 4.64.13
 )
 
 "build\tools\FAKE.Core\tools\Fake.exe" build.fsx %*
