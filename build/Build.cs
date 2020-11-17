6db72f0930c543cde95c995778dfb94992efa870
Alex Povar <zvirja1@gmail.com>
2020-11-17 16:12:51 +0100

    Update Nuke and maintain buildscripts

--- build/Build.cs
+++ build/Build.cs
@@ -25,7 +25,6 @@ using static Nuke.Common.Tools.Git.GitTasks;
 [UnsetVisualStudioEnvironmentVariables]
 class Build : NukeBuild
 {
-
     public static int Main () => Execute<Build>(x => x.CompleteBuild);
 
     [Solution] readonly Solution Solution;
@@ -65,10 +64,8 @@ class Build : NukeBuild
 
             CurrentBuildVersion = BuildVersionParam switch
             {
-                "git" => CalculateVersionFromGit(),
-
+                "git" => GitVersioning.CalculateVersionFromGit(BuildNumberParam),
                 "dev" => CalculateDevVersion(),
-
                 var ver => new BuildVersionInfo {AssemblyVersion = ver, FileVersion = ver, InfoVersion = ver, NuGetVersion = ver}
             };
             
@@ -87,51 +84,6 @@ class Build : NukeBuild
 
                 var devVersion = $"1.0.0.{currentSeed}";
                 return new BuildVersionInfo {AssemblyVersion = "1.0.0", FileVersion = "1.0.0", InfoVersion = devVersion, NuGetVersion = devVersion};
-            }
-            
-            BuildVersionInfo CalculateVersionFromGit()
-            {
-                var desc = Git("describe --tags --long --abbrev=40 --match=v*", logInvocation: false, logOutput: false).Single().Text;
-                var result = Regex.Match(desc,
-                        @"^v(?<maj>\d+)\.(?<min>\d+)(\.(?<rev>\d+))?(?<pre>-\w+\d*)?-(?<num>\d+)-g(?<sha>[a-z0-9]+)$",
-                        RegexOptions.IgnoreCase)
-                    .Groups;
-
-                string GetMatch(string name) => result[name].Value;
-
-                var major = int.Parse(GetMatch("maj"));
-                var minor = int.Parse(GetMatch("min"));
-                var optRevision = GetMatch("rev");
-                var optPreReleaseSuffix = GetMatch("pre");
-                var commitsNum = int.Parse(GetMatch("num"));
-                var sha = GetMatch("sha");
-
-                var revision = optRevision == "" ? 0 : int.Parse(optRevision);
-                var assemblyVersion = $"{major}.{minor}.{revision}.0";
-                var fileVersion = $"{major}.{minor}.{revision}.{BuildNumberParam}";
-                
-                // If number of commits since last tag is greater than zero, we append another identifier with number of commits.
-                // The produced version is larger than the last tag version.
-                // If we are on a tag, we use version specified modification.
-                var nugetVersion = commitsNum switch
-                {
-                    0 => $"{major}.{minor}.{revision}{optPreReleaseSuffix}",
-                    var num => $"{major}.{minor}.{revision}{optPreReleaseSuffix}.{num}",
-                };
-                
-                var infoVersion = commitsNum switch
-                {
-                    0 => nugetVersion,
-                    _ => $"{nugetVersion}-{sha}"
-                };
-                
-                return new BuildVersionInfo
-                {
-                    AssemblyVersion = assemblyVersion,
-                    FileVersion = fileVersion,
-                    InfoVersion = infoVersion,
-                    NuGetVersion = nugetVersion
-                };
             }
         });
 
@@ -180,13 +132,13 @@ class Build : NukeBuild
             var testProject = Solution.GetProject(TestProjectName);
             
             var testAssemblyPath = OutputDir / $"{TestProjectName}.dll";
-            
+
             NUnit3(c => c
                 .AddInputFiles(testAssemblyPath)
                 .SetResults(TestResultFile)
                 .SetFramework("net-4.5")
                 .SetTimeout(30_000)
-                .AddEnvironmentVariable("JetProductHomeDir", testProject!.Directory)
+                .AddProcessEnvironmentVariable("JetProductHomeDir", testProject!.Directory)
             );
         });
 
@@ -261,15 +213,12 @@ class Build : NukeBuild
 
             var testResultBytes = await File.ReadAllBytesAsync(TestResultFile);
 
-            using var httpClient = new HttpClient();
-            // httpClient.BaseAddress = new Uri(env.ApiUrl, UriKind.Absolute);
-            // Info($"Api URL: {httpClient.BaseAddress}");
-
             using var multipartContent = new MultipartFormDataContent($"------------{DateTime.Now.Ticks:X}")
             {
                 {new ByteArrayContent(testResultBytes), "file", Path.GetFileName(TestResultFile)}
             };
- 
+            
+            using var httpClient = new HttpClient();
             var result = await httpClient.PostAsync($"https://ci.appveyor.com/api/testresults/nunit3/{AppVeyorEnv.JobId}", multipartContent);
             result.EnsureSuccessStatusCode();
             Info($"Successfully uploaded the file");
@@ -282,7 +231,7 @@ class Build : NukeBuild
             var trigger = ResolveAppVeyorTrigger();
             if (trigger != AppVeyorTrigger.PR)
             {
-                AppVeyorEnv.UpdateBuildNumber(CurrentBuildVersion.FileVersion);
+                AppVeyorEnv.UpdateBuildVersion(CurrentBuildVersion.FileVersion);
                 Info($"Updated build version to: '{CurrentBuildVersion.FileVersion}'");
             }
         });
@@ -321,7 +270,7 @@ class Build : NukeBuild
         }
 
         var tag = env.RepositoryTag ? env.RepositoryTagName : null;
-        var isPr = env.PullRequestNumber != 0;
+        var isPr = env.PullRequestNumber != null;
         var branchName = env.RepositoryBranch;
         
         return (tag, isPr, branchName) switch
